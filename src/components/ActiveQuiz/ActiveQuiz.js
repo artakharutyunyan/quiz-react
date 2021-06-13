ec790758d8d88a830974cfe489bb1d92caa47a99
Artak Harutyunyan <artak_harutyunyan@mail.com>
2021-06-13 18:44:26 +0400

    Minor fix

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import classes from './ActiveQuiz.css'
-import AnswersList from './AnswersList/AnswersList'
+import React from "react";
+import classes from "./ActiveQuiz.css";
+import AnswersList from "./AnswersList/AnswersList";
 
-const ActiveQuiz = props => {
+const ActiveQuiz = (props) => {
   return (
     <div className={classes.ActiveQuiz}>
       <p className={classes.Question}>
-      <span>
-        <strong>{props.answerNumber}.</strong>&nbsp;
-        {props.question}
-      </span>
+        <span>
+          <strong>{props.answerNumber}.</strong>&nbsp;
+          {props.question}
+        </span>
 
-        <small>{props.answerNumber} из { props.quizLength }</small>
+        <small>
+          {props.answerNumber} from {props.quizLength}
+        </small>
       </p>
 
       <AnswersList
@@ -20,7 +22,7 @@ const ActiveQuiz = props => {
         onAnswerClick={props.onAnswerClick}
       />
     </div>
-  )
-}
+  );
+};
 
-export default ActiveQuiz
+export default ActiveQuiz;
